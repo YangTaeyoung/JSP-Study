771b36b90761008797fcd466d8452ff7fd62590f
YangTaeyoung <59782504+YangTaeyoung@users.noreply.github.com>
2020-03-27 02:00:39 +0900

    Create HelloServlet.java

---
+++ 서블릿/HelloServlet/HelloServlet.java
@@ -0,0 +1,65 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet") // 이게 요청 주소가 된다.
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		
+		out.println("<head>");
+		out.println("<title>Hello.Servlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h2>Helloservlet!</h2>");
+		out.println("<h3>Message from doGet method!</h3>");
+		out.println("</body>");
+		out.println("</html>");
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Hello.Servlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h2>HelloServlet!</h2>");
+		out.println("<h3>Message from doPost method!</h3>");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+
+}
